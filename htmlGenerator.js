deac9bda38fb3c5edc5b3ed44998fcde28c8c679
Kenta Kusakabe <k.kusa0828@gmail.com>
2023-05-06 10:46:32 +0100

    file refresh

--- htmlGenerator.js
+++ htmlGenerator.js
@@ -1,14 +1,14 @@
 const currentWeather = document.getElementById("currentWeatherContainer");
 
 //Get current weather
-export const getCurrentWeather = (weatherData) => {
+export const getCurrentWeather = (data) => {
   currentWeather.innerHTML = `<h2>Current Weather</h2>
                               <img src="https://openweathermap.org/img/wn/${
-                                weatherData.weather[0].icon
+                                data.weather[0].icon
                               }@2x.png" alt="weather icon">
-                              <p>${weatherData.weather[0].description}</p>
+                              <p>${data.weather[0].description}</p>
                               <p>Temp: ${Math.round(
-                                weatherData.main.temp - 273.15
+                                data.main.temp - 273.15
                               )}&#8451;</p>
-                              <p>Humidity: ${weatherData.main.humidity}%</p>`;
+                              <p>Humidity: ${data.main.humidity}%</p>`;
 };
