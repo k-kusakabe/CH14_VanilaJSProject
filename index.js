29db5be2b88e3567dd9670c637a5a291f25f4b4a
Kenta Kusakabe <k.kusa0828@gmail.com>
2023-05-06 10:25:57 +0100

    keydown event added

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const main = document.getElementById("main");
 const currentWeather = document.getElementById("currentWeatherContainer");
 const hourlyWeather = document.getElementById("hourlyWeatherContainer");
 
+// get current weather by button click
 button.addEventListener("click", async () => {
   const value = cityName.value;
   try {
@@ -20,6 +21,26 @@ button.addEventListener("click", async () => {
       currentWeather.innerHTML = `<h3 class="errorMessage1">City name is invalid. Enter a valid name</h3>`;
     } else {
       currentWeather.innerHTML = `<h3 class="errorMessage2">Currently, weather info cannot be accessed. Please visit the site later</h3>`;
+    }
+  }
+});
+
+// get current weather by Enter key
+cityName.addEventListener("keydown", async (e) => {
+  if (e.key === "Enter") {
+    const value = cityName.value;
+    try {
+      const currentWeatherAPI = `https://api.openweathermap.org/data/2.5/weather?q=${value}&appid=${apiKey}`;
+      const { data } = await axios.get(currentWeatherAPI);
+      console.log(data);
+      getCurrentWeather(data);
+    } catch (error) {
+      console.log(error);
+      if (error.response) {
+        currentWeather.innerHTML = `<h3 class="errorMessage1">City name is invalid. Enter a valid name</h3>`;
+      } else {
+        currentWeather.innerHTML = `<h3 class="errorMessage2">Currently, weather info cannot be accessed. Please visit the site later</h3>`;
+      }
     }
   }
 });
